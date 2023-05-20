65cd62b36ed3c08674f894d8af17ff8033ef4099
Ramir Salcedo <ramir101@gmail.com>
2023-05-20 17:32:08 -0400

    html added

--- server/app.ts
+++ server/app.ts
@@ -7,4 +7,6 @@ app.use(express.urlencoded({ extended: false }));
 const path = require("path");
 app.use("/build", express.static(path.join(__dirname, "../../build")));
 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "../../index.html"))
+);
