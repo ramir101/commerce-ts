25b64d4838e845e6b32bc233a8f25094197a216f
Ramir Salcedo <ramir101@gmail.com>
2023-05-20 17:28:06 -0400

    setup done

---
+++ server/app.ts
@@ -0,0 +1,10 @@
+import express from "express";
+export const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const path = require("path");
+app.use("/build", express.static(path.join(__dirname, "../../build")));
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
