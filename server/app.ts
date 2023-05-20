a00c5ba3ae79230341ef996b69c29b4569271b31
Ramir Salcedo <ramir101@gmail.com>
2023-05-20 17:34:52 -0400

    setting up for db

--- server/app.ts
+++ server/app.ts
@@ -6,7 +6,8 @@ app.use(express.urlencoded({ extended: false }));
 
 const path = require("path");
 app.use("/build", express.static(path.join(__dirname, "../../build")));
+app.use("/static", express.static(path.join(__dirname, "../../static")));
 
 app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "../../index.html"))
+  res.sendFile(path.join(__dirname, "../../static/index.html"))
 );
