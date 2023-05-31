14dbdae42ff3c1ffafce01876f50edc868bb2675
Ramir Salcedo <ramir101@gmail.com>
2023-05-30 20:46:02 -0400

    product setup completed

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import { App } from "./components/App";
 import store from "./store";
 import { Provider } from "react-redux";
+import { HashRouter } from "react-router-dom";
 
 const root = createRoot(document.querySelector("#root")!);
 
 root.render(
   <Provider store={store}>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </Provider>
 );
