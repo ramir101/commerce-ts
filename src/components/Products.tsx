65262ba81777c8b493abb7bfeff98077de24f9c0
Ramir Salcedo <ramir101@gmail.com>
2023-07-11 20:11:56 -0400

    category filter added

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { ProductI, RootState } from "../store";
-import { Grid } from "@mui/material";
+import { Box, Grid, Tab, Tabs, Typography } from "@mui/material";
 import Product from "./Product";
 import { useParams } from "react-router-dom";
 
@@ -13,7 +13,10 @@ function Products() {
   ) as ProductI[];
 
   const { category } = useParams();
-  const displayedProducts = products.filter((product) => {
+
+  const [value, setValue] = React.useState("all");
+
+  let displayedProducts = products.filter((product) => {
     if (category === "dogs" && product.petType === "dog") {
       return product;
     }
@@ -25,6 +28,28 @@ function Products() {
     }
   });
 
+  const handleTabChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+
+  if (value !== "all") {
+    if (value === "Accessories") {
+      displayedProducts = displayedProducts.filter(
+        (product) => product.category === "accessory"
+      );
+    }
+    if (value === "Food") {
+      displayedProducts = displayedProducts.filter(
+        (product) => product.category === "food"
+      );
+    }
+    if (value === "Toys") {
+      displayedProducts = displayedProducts.filter(
+        (product) => product.category === "toy"
+      );
+    }
+  }
+
   return (
     <Grid container>
       <Grid
@@ -33,8 +58,18 @@ function Products() {
         direction="row"
         justifyContent="center"
         alignItems="center">
-        <Grid item>
-          <h1>{category?.toUpperCase()}</h1>
+        <Grid item xs={12} sm={12} md={12} lg={12}>
+          <Typography variant="h2" align="center">
+            {category?.toUpperCase()}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={12} paddingBottom={"10px"}>
+          <Tabs value={value} onChange={handleTabChange} centered>
+            <Tab label="All" value={"all"} />
+            <Tab label="Food" value={"Food"} />
+            <Tab label="Toys" value={"Toys"} />
+            <Tab label="Accessories" value={"Accessories"} />
+          </Tabs>
         </Grid>
       </Grid>
       <Grid container item spacing={2}>
